31247635cff819cbcd58267c1d88bd4dfd99d738
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-03-29 21:24:25 -0400

    Added 'x' icon and css styling

--- js/main.js
+++ js/main.js
@@ -5,14 +5,15 @@ let $newSkill = $('<tr><td id="skills"><label><input type="checkbox"> Cascading 
 
 $('button').on('click', function(e){
     let $enterSkill = $('.skill-field').val();
-    let skillString = `<tr class="skills"><td><label><input type="checkbox"> ${$enterSkill}</label></td></tr>`;
+    let skillString = `<tr class="skills"><td><span class="delete">X</span>${$enterSkill}</td></tr>`;
+    
     $('tbody').append(skillString);
     $enterSkill = $('.skill-field').val('');
     // document.querySelector('.skill-field').value = "";
     // console.log(enterSkill);
 });
 
-$('tbody').on('click', 'tr', function(evt) {
-    const ss = $(this).remove();
+$('tbody').on('click', 'span', function(evt) {
+    const ss = $(this).closest('tr').remove();
     ss.remove();
 });
